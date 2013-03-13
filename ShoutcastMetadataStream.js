11cab35929d58867efcef6fa4a3e38af58cca9a9
Mark Schmale <masch@masch.it>
2013-03-13 19:39:46 +0100

    added Metadata support

---
+++ ShoutcastMetadataStream.js
@@ -0,0 +1,45 @@
+var Transform = require('stream').Transform
+  , moore     = require('./moore')
+
+function ShoutcastMetadataStream(options)
+{
+    if(!(this instanceof ShoutcastMetadataStream)) {
+        return new ShoutcastMetadataStream(options)
+    }
+    Transform.call(this, options)
+    this.metaint = options.metaint;
+    this.count   = 0;
+}
+
+ShoutcastMetadataStream.prototype = Object.create(
+    Transform.prototype, {
+        constructor: {
+            value: ShoutcastMetadataStream
+        }
+    }
+)
+var first = true;
+ShoutcastMetadataStream.prototype._transform = function(chunk, push, done)
+{
+    if(this.count + chunk.length >= this.metaint) {
+        var skip = this.metaint-this.count;
+        if((chunk[skip]*16) > 0 ) {
+            var split = chunk.slice(skip+1, skip+(chunk[skip]*16)+1);
+            this.emit('metadata', split.toString());
+        }
+        var neuchunk = Buffer.concat([chunk.slice(0, skip), chunk.slice(skip+(chunk[skip]*16)+1)]);
+        push(neuchunk)
+        this.count = chunk.length - (skip+1+(chunk[skip]*16));
+    }
+    else {
+        push(chunk)
+        this.count += chunk.length;
+    }
+    done()
+}
+
+ShoutcastMetadataStream.prototype.destroy = function()
+{
+}
+
+module.exports = ShoutcastMetadataStream
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var express = require('express')
   , fs      = require('fs')
   , http    = require('http')
   , ffmpeg  = require('fluent-ffmpeg')
+  , shoutc  = require('./ShoutcastMetadataStream')
 
 // serve station list
 app.get('/stations.json', function(req, res) {
@@ -12,12 +13,41 @@ app.get('/stations.json', function(req, res) {
 app.get('/', function(req, res) {
   fs.createReadStream('./public/index.html').pipe(res)
 })
+
+var streamTitle = {
+
+}
+app.get('/data/:name.json', function(q, s) {
+  console.log('data for ', q.params.name);
+  s.json(streamTitle[q.params.name] || 'no information');
+  console.log('answers here!');
+})
+
 // ogg all teh steams!
 app.get('/stream/:name.ogg', function(req, res) {
   res.contentType('ogg')
-  http.get('http://pub3.rockradio.com:80/rr_' + req.params.name, function(inc) {
+  http.get({
+    'host': 'pub3.rockradio.com',
+    'path': '/rr_' + req.params.name,
+    'headers': {
+      'icy-metadata': 1
+    }
+  },
+  function(inc) {
+    var metaparser = new shoutc({ metaint: parseInt(inc.headers['icy-metaint'], 10) });
+    inc.pipe(metaparser)
+    var channel = req.params.name;
+    metaparser.on('metadata', function(data) {
+      streamTitle[channel] = {}
+      data.split(/;/).forEach(function(line) {
+        var x = line.split(/=/);
+        if(x.length != 2)
+          return;
+        streamTitle[channel][x[0]] = x[1].replace(/^'|'$/g, '');
+      });
+    });
     var proc = new ffmpeg({
-        source: inc,
+        source: metaparser,
         timeout: 10*60*60,
         nolog: true
       })
@@ -25,7 +55,10 @@ app.get('/stream/:name.ogg', function(req, res) {
       .withAudioBitrate('96k')
       .withAudioChannels(2)
       .withAudioCodec('libvorbis')
-      .writeToStream(res)
+      .writeToStream(res, function() {
+        inc.destroy()
+      })
+
   });
 })
 // profit!
